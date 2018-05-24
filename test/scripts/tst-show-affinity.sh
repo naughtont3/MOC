421e09f74b2d5d330e4f006a59256c8393fbae61
Thomas Naughton <naughtont@ornl.gov>
2018-05-24 17:26:16 -0400

    Update 'tst-show-affinity.sh' script

--- test/scripts/tst-show-affinity.sh
+++ test/scripts/tst-show-affinity.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 ORTERUN=orterun
-ORTERUN_ARGS=" --report-bindings --map-by node --bind-to core "
-ORTERUN_NPROCS="--np 2 "
+#ORTERUN_ARGS=" --report-bindings --map-by node --bind-to core "
+ORTERUN_ARGS=" --report-bindings --map-by node --bind-to socket "
+ORTERUN_NPROCS="--np 4 "
 
-EXE=../tst-show-affinity
+EXE=./tst-show-affinity
 EXE_ARGS=
 #EXE_ARGS="-v"
 
@@ -65,5 +66,21 @@ echo "########################################################"
 $cmd 
 
 #---------------------
-# TODO: With Binding, with CUSTOM places
+# With Binding, with CUSTOM places
+#       (below are the same syntax, diff format)
+# TODO: Not sure this is doing same PLACES as above,
+#       also gets warning msg,
+#         libgomp: Number of places reduced from 4 to 1 because some places didn't
+#         contain any usable logical CPUs
 
+#OMP_ARGS="env OMP_PROC_BIND=true  OMP_NUM_THREADS=8 OMP_PLACES={1:8},{13:8},{25:8},{37:8}"
+OMP_ARGS="env OMP_PROC_BIND=true  OMP_NUM_THREADS=8 OMP_PLACES={1:8}:4:12"
+#TJN-DBG: OMP_ARGS="env OMP_NUM_THREADS=8"
+cmd="$OMP_ARGS $ORTERUN $ORTERUN_ARGS $ORTERUN_NPROCS $EXE $EXE_ARGS"
+
+echo ""
+echo "########################################################"
+echo "# CMD: $cmd"
+echo "# NOTE: With Binding, with places"
+echo "########################################################"
+$cmd
